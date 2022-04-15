c861c40bc7682ddf7e17960fdfd31880bb3b5939
radarxy <amrutanshutyagi@gmail.com>
2022-04-16 00:30:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require("dotenv").config()
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,6 +18,12 @@ client.on("messageCreate", (message) => {
     if (message.content == "ping") {
         message.reply("pong")
     }
+})
+
+const welcomechannelID = "964573758293704704"
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomechannelID).send(`<@${member.id}> welcome to the server`)
 })
 
 client.login(process.env.TOKEN)
